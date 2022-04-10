0b16d9c055624be9e86cf5faaab945a6df1e5c5c
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-04-10 21:37:47 +0900

    Initial Commit

---
+++ swift-tutorial/swift_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_tutorialApp.swift
+//  swift-tutorial
+//
+//  Created by apple developer academy on 2022/04/10.
+//
+
+import SwiftUI
+
+@main
+struct swift_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
